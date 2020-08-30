fc23704e75a449e0ac7fb83d6d19bdb729750e69
Tomas Sirio <tomassirio@Tomass-MacBook-Air.local>
2020-08-30 01:34:42 -0300

    fixed a bug with the Help command

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 const Util = require("../utils/utils.js");
-const { prefix } = require('../Config/config.json');
+require("dotenv").config()
 
 module.exports = {
     name: 'help',
@@ -7,13 +7,13 @@ module.exports = {
     execute(message, args){
         var channel = message.channel
 
-        var msg = "**"+prefix+"add {element}** - to add an element to the list\n"
-        msg    += "**"+prefix+"remove {element}** - to remove an element from the list\n"
-        msg    += "**"+prefix+"list** - to list every element on the list\n"
-        msg    += "**"+prefix+"random** - gets a random element from the list\n"
-        msg    += "**"+prefix+"poll** - creates a poll on the channel for 5 random elements. WIP\n"
-        msg    += "**"+prefix+"log** - gets the bot's log\n"
-        msg    += "**"+prefix+"help** - to see this message\n"
+        var msg = "**"+process.env.PREFIX+"add {element}** - to add an element to the list\n"
+        msg    += "**"+process.env.PREFIX+"remove {element}** - to remove an element from the list\n"
+        msg    += "**"+process.env.PREFIX+"list** - to list every element on the list\n"
+        msg    += "**"+process.env.PREFIX+"random** - gets a random element from the list\n"
+        msg    += "**"+process.env.PREFIX+"poll** - creates a poll on the channel for 5 random elements. WIP\n"
+        msg    += "**"+process.env.PREFIX+"log** - gets the bot's log\n"
+        msg    += "**"+process.env.PREFIX+"help** - to see this message\n"
 
         var embededMessage = Util.embedMessage("List Bot Help Message", "0xffff00", msg)
         channel.send(embededMessage);
