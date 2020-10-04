a244b13001a83710ae23340765351a67c46d70e1
Javier Ledezma <jledezma@medtrainer.com>
2020-10-04 13:05:14 -0500

    [Hotfix] Prevent fatal exception if channel is not defined
    Signed-off-by: Javier Ledezma <jledezma@medtrainer.com>

--- commands/list.js
+++ commands/list.js
@@ -6,29 +6,40 @@ module.exports = {
     description: 'Lists all the elements of the list',
     execute: async (message, args) => {
         let channel = message.channel
-            
+
         let msg = ""
 
-        const settings = await Channel.findOne({
-            channelId: channel.id
-        }, (err, foundChannel) => {
-            if(!foundChannel){
-                const newChannel = new Channel({
+        try {
+            let foundChannel = await Channel.findOne({
+                channelId: channel.id
+            });
+
+            if(!foundChannel) {
+                const foundChannel = new Channel({
                     channelId: channel.id,
                     server: channel.guild.name,
                     name: channel.name,
                     items: []
                 })
-                newChannel.save()
-                .then(result => console.log(result))
-                .catch(err => console.error(err))
+
+                foundChannel.save()
+                  .then(result => console.log(result))
+                  .catch(err => console.error(err))
             }
-            let i = 0
-            for (let item of foundChannel.items) {
-                msg += "[" + i + "]\t" + item.content + "\t\t" + item.author + "\n"
-                i++
+
+            if (! foundChannel.items || foundChannel.items.length === 0) {
+                msg = "No items found, please use the 'add {element}' command to put your first item."
+            } else {
+                let i = 0
+                for (let item of foundChannel.items) {
+                    msg += "[" + i + "]\t" + item.content + "\t\t" + item.author + "\n"
+                    i++
+                }
             }
-        })
+        } catch (e) {
+            console.log(e)
+            msg = "Unable to generate the list, please try again."
+        }
 
         console.log(msg)
         let embededMessage = Util.embedMessage(channel.name + " list", "0xffff00", msg)
