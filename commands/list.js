06c5033f55e333387936c815a869a6f6810027ca
Nathan Dawson <nathan.dawson@orderpay.com>
2020-10-04 19:21:31 +0100

    fix bad conflict resolve

--- commands/list.js
+++ commands/list.js
@@ -27,19 +27,15 @@ module.exports = {
                   .then(result => console.log(result))
                   .catch(err => console.error(err))
             }
-            let i = 0
-            for (let item of foundChannel.items) {
-                fields.push({ 
-                    name: `${i} - ${item.author}`,
-                    value: item.content
-                })
-                i++
             if (! foundChannel.items || foundChannel.items.length === 0) {
                 msg = "No items found, please use the 'add {element}' command to put your first item."
             } else {
                 let i = 0
                 for (let item of foundChannel.items) {
-                    msg += "[" + i + "]\t" + item.content + "\t\t" + item.author + "\n"
+                    fields.push({ 
+                        name: `${i} - ${item.author}`,
+                        value: item.content
+                    })
                     i++
                 }
             }
