4f59d08edd566abbee5f3899cd1eeea0973c3fab
Akshansh Rewariya <akshanshrewariya@gmail.com>
2020-10-05 22:09:20 +0530

    Multi-Add Command added

---
+++ src/commands/multi-add.js
@@ -0,0 +1,38 @@
+const Util = require("../utils/utils.js");
+const Channel = require("../models/Channel")
+const Item = require("../models/Item.js")
+const ChannelRepository = require("../repositories/channel-repository")
+
+module.exports = {
+    name: 'multi-add',
+    description: 'Add multiple elements to the list',
+
+    execute: async (message, args) => {
+        let item = [];
+
+        args.forEach(element=>{
+            item.push(element);
+        });
+
+        let channel = message.channel
+
+        const dbChannel = await ChannelRepository.findOrCreate(channel);
+
+        item.forEach(async (instance)=>{
+
+        let newItem = new Item({
+            content: instance,
+            author: message.author.username,
+        });
+
+            dbChannel.items.push(newItem)
+        })
+        
+        dbChannel.save();
+
+        let embededMessage = Util.embedMessage("Succesfully added", "0xffff00", item)
+        channel.send(embededMessage);
+
+    },
+};
+
