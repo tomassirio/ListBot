0f3be26aa1f19266afb6e9b82370a71cbcad03ba
Javier Ledezma <jledezma@medtrainer.com>
2020-10-04 21:04:12 -0500

    [Hotfix] Fix empty list message after change the list format

--- src/commands/list.js
+++ src/commands/list.js
@@ -4,22 +4,25 @@ const ChannelRepository = require("../repositories/channel-repository")
 module.exports = {
     name: 'list',
     description: 'Lists all the elements of the list',
-    execute: async (message, args) => {
+    execute: async (message) => {
         let channel = message.channel
         let fields = []
         let dbChannel = await ChannelRepository.findOrCreate(channel)
 
         if (! dbChannel.items || dbChannel.items.length === 0) {
-            msg = "No items found, please use the 'add {element}' command to put your first item."
-        } else {
-            let i = 0
-            for (let item of dbChannel.items) {
-                fields.push({
-                    name: `${i} - ${item.author}`,
-                    value: item.content
-                })
-                i++
-            }
+            const emptyMessage = Util.embedMessage('Warning', '0xffff00', "No items found, please use the 'add {element}' command to put your first item.")
+            channel.send(emptyMessage)
+
+            return
+        }
+
+        let i = 0
+        for (let item of dbChannel.items) {
+            fields.push({
+                name: `${i} - ${item.author}`,
+                value: item.content
+            })
+            i++
         }
 
         let embededMessage = Util.generateListEmbed(channel.name + " list", "0xffff00", fields, `Requested by ${message.author.username}`)
