d1fd6eff1b864e5c05dd8fdb99780437cf99b954
Aldo Turmon <aldoturegano10@hotmail.com>
2020-10-08 16:32:33 -0500

    Deleting element from DB

--- src/commands/remind.js
+++ src/commands/remind.js
@@ -1,6 +1,5 @@
 const Util = require("../utils/utils.js");
-const Item = require("../models/Item.js")
-const ChannelRepository = require("../repositories/channel-repository")
+const Item = require("../models/Item.js");
 
 module.exports = {
     name: 'remind',
@@ -12,15 +11,6 @@ module.exports = {
             item += args[i] + " "
         }
         let channel = message.channel
-
-        const newItem = new Item({
-            content: item,
-            author: message.author.tag
-        })
-
-        const dbChannel = await ChannelRepository.findOrCreate(channel)
-        dbChannel.items.push(newItem)
-        dbChannel.save()
 
         let embededMessage = Util.embedMessage(`I've succesfully added your element to the list and will remind you in ${time} minutes.` , message.author.tag, "0xffff00", item)
         channel.send(embededMessage);
