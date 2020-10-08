3eaa583bad5da726d776655f7b27afa3e91222e3
Aldo Turmon <aldoturegano10@hotmail.com>
2020-10-08 18:19:01 -0500

    Changing CHANGELOG file path

--- src/commands/log.js
+++ src/commands/log.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'Gets info from the bot',
     execute: async (message, args) => {
         let channel = message.channel
-        const log = await fs.readFile(path.join(__dirname, '../CHANGELOG'))
+        const log = await fs.readFile(path.join(__dirname, '../../CHANGELOG'))
 
         let embededMessage = Util.embedMessage("Log version" , message.author.tag , "0xff0000", log)
         channel.send(embededMessage)
