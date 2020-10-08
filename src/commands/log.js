b68cdea4125796050b2aeaa389468446f82f35fd
Tomas Sirio <tomassirio@gmail.com>
2020-10-08 12:22:08 -0300

    Update log.js

--- src/commands/log.js
+++ src/commands/log.js
@@ -11,6 +11,7 @@ module.exports = {
         msg += "1.0.1 - Added a random function. Added a confirmation on the Add and Remove Function\n"
         msg += "1.1.0 - Added the Poll function. WIP\n"
         msg += "2.0   - Bot was completely revamped. Database now functioning\n"
+        msg += "3.0   - Bot got spookier on #Hacktoberfest2020. Added Add-Many, Remove-Many, Clear and Remind commands. Also lots of bug fixes and code optimization
 
         let embededMessage = Util.embedMessage("Log version" , message.author.tag , "0xff0000", msg)
         channel.send(embededMessage)
