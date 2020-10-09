34c720a70505853c832d3ae9674cd8f1e4aee891
medusalix <ss7@live.de>
2020-10-09 15:34:45 +0200

    Fix config

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@ if(result.error) throw result.error; // Throw an error if it failed to load the 
 module.exports = {
     prefix : process.env.PREFIX,
     botToken : process.env.BOT_TOKEN,
-    devMongoUrl : `mongodb://${process.env.LOCAL_DB_USER}:${process.env.LOCAL_DB_PASSWORD}@cluster0-shard-00-00.xeg5k.mongodb.net:27017,cluster0-shard-00-01.xeg5k.mongodb.net:27017,cluster0-shard-00-02.xeg5k.mongodb.net:27017/${process.env.LOCAL_DB_MONGO}?ssl=true&replicaSet=atlas-ibftvd-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    devMongoUrl : `mongodb://${process.env.LOCAL_DB_USER}:${process.env.LOCAL_DB_PASSWORD}@mongo/${process.env.LOCAL_DB_MONGO}`,
     productionMongoURL : `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xeg5k.mongodb.net/${process.env.DB_MONGO}?retryWrites=true&w=majority`
 }
 
