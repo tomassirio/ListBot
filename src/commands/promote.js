b36e6b532ab58ea9af15637864b6cb27b232ddaf
Ahmad Reza <hungry.dev39@gmail.com>
2020-10-19 19:55:20 +0700

    add new command to promote user into an admin of bot

---
+++ src/commands/promote.js
@@ -0,0 +1,31 @@
+const Guild = require('../models/guild');
+const Util = require('../utils/utils.js')
+
+module.exports = {
+  name: 'promote',
+  description: 'Promote a user into an admin of ListBot',
+  execute: async (message, args) => {
+    // check if user promoting is admin
+    const guildID = message.guild.id;
+    const authorId = message.author.id;
+
+    const guild = await Guild.findOne({ guildID });
+
+    const authorIsAdmin = guild.guildAdminsId.some(adminId => adminId === authorId);
+
+    if (!authorIsAdmin) return;
+    // get mentioned user id
+    const mentionedUserId = message.mentions.users.first().id;
+
+    // add id into mongo guild schema
+    guild.guildAdminsId.push(mentionedUserId);
+    guild.save();
+    let embededMessage = Util.embedMessage(
+      'Succesfully added user as admin',
+      message.author.tag,
+      '0xffff00',
+      'Hooray',
+    );
+    message.channel.send(embededMessage);
+  },
+};
