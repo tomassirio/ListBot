b1540f66096b7b9687e074e46d9f983e1e9a5285
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2020-10-24 04:11:37 +0530

    Standardized the embed format

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,14 +1,13 @@
 const Discord = require('discord.js')
 
 const Utils = {
-    embedMessage(message, author, color, item) {
-        const embed = new Discord.MessageEmbed()
-            .setTitle(message)
+    embedMessage(title, author, color, description) {
+        return new Discord.MessageEmbed()
+            .setTitle(title)
             .setColor(color)
-            .setDescription(item)
-            .setFooter(author)
+            .setDescription(description)
+            .setFooter(author.username, author.displayAvatarURL())
             .setTimestamp()
-        return embed
     },
     generateListEmbed(title, color, fields, footer) {
         const embed = new Discord.MessageEmbed({
