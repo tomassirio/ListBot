d300e682f711e0610eeb277a4a752a6bc43687f2
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2020-10-24 07:51:18 +0530

    List command uses codeblocks within embeds.
    Need more formatting for the content within codeblocks

--- src/commands/list.js
+++ src/commands/list.js
@@ -20,19 +20,18 @@ module.exports = {
             return
         }
 
-        let fields = dbChannel.items.map((item, i) => ({
-            name: `${i + 1} - ${item.content}`,
-            value: item.author,
-        }))
+        let listItems = dbChannel.items.map(
+            (item, i) => `${i + 1}) ${item.content}    -    '${item.author}'`
+        )
 
         let channelName = channel.name
         channelName = channelName.charAt(0).toUpperCase() + channelName.slice(1) // Capitalize the first letter in the channel name.
 
-        let embeddedMessage = Util.generateListEmbed(
-            `${channelName} List`,
+        let embeddedMessage = Util.embedMessage(
+            `List for \`${channelName}\``,
+            message.author,
             '0xffff00',
-            fields,
-            `Requested by ${message.author.tag}`
+            `\`\`\`nim\n${listItems.join('\n')}\`\`\``
         )
         channel.send(embeddedMessage)
     },
