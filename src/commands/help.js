0254459b877e153a4aacc6a87794ea2abb8072cd
unknown <losdingtro@gmail.com>
2020-12-15 20:39:20 +0100

    Modify help command to search for command files

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,6 +1,9 @@
 const config = require('../config')
 const Util = require('../utils/utils.js')
 const Style = require('../utils/messageStyle.js')
+const fs = require('fs')
+
+const COMMANDS_FOLDER = './src/commands'
 
 module.exports = {
     name: 'help',
@@ -9,32 +12,15 @@ module.exports = {
         let { channel } = message
 
         // [syntax, description]
-        let commands = [
-            ['add {element}', 'to add an element to the list'],
-            [
-                'multi-add {element} {element}',
-                'to add multiple elements to the list',
-            ],
-            ['remove {element}', 'to remove an element from the list'],
-            [
-                'multi-remove {element} {element}',
-                'to remove multiple elements from the list',
-            ],
-            ['list', 'to list every element on the list'],
-            ['random', 'gets a random element from the list'],
-            [
-                'poll {active_time_in_minutes} [{number_of_items}]',
-                'creates a poll on the channel for 2 to 9 random elements. ' +
-                    'Uses 5 elements by default. If you want a poll with custom number of elements ' +
-                    'with no poll time limit, set the time as 0.',
-            ],
-            ['log', "gets the bot's log"],
-            [
-                'remind {time_in_minutes} {element}',
-                'to add an item and be reminded in n minutes',
-            ],
-            ['help', 'to see this message'],
-        ]
+        let commands = []
+
+        fs.readdirSync(COMMANDS_FOLDER).forEach(file => {
+            const command = require('./' + file)
+            if (!command.hasOwnProperty('name') || !command.hasOwnProperty('description')) return
+
+            const usage = (command.usage != null) ? ' ' + command.usage : ''
+            commands.push([`${command.name}${usage}`, command.description])
+        })
 
         let msg = commands
             .map(
